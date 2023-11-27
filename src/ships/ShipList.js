1c004c6983be4d39018e5d2e725f38632f3a18ac
NeoUKR <81991143+NeoUKR@users.noreply.github.com>
2023-11-27 11:53:45 +0200

    1. DOCKER: Append venvs for API 2. REACT-ADMIN: Append show ships

--- src/ships/ShipList.js
+++ src/ships/ShipList.js
@@ -1,4 +1,4 @@
-import React, { Children, Fragment, cloneElement, memo } from 'react';
+import React, { Children, Fragment, cloneElement, memo, useEffect, useState } from 'react';
 import BookIcon from '@material-ui/icons/Book';
 import { Grid } from '@mui/material'
 import Chip from '@material-ui/core/Chip';
@@ -10,7 +10,6 @@ import {
     BulkDeleteButton,
     BulkExportButton,
     ChipField,
-    CardActions,
     Datagrid,
     DateField,
     downloadCSV,
@@ -30,47 +29,14 @@ import {
 
 import ResetViewsButton from './ResetViewsButton';
 
-import { MapContainer, TileLayer } from "react-leaflet";
+import { useMap, MapContainer, TileLayer, GeoJSON, LayersControl, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 import "./styles.css";
 import "leaflet/dist/leaflet.css";
 
 export const PostIcon = BookIcon;
 
-// const useQuickFilterStyles = makeStyles(theme => ({
-//     chip: {
-//         marginBottom: theme.spacing(1),
-//     },
-// }));
-// const QuickFilter = ({ label }) => {
-//     const translate = useTranslate();
-//     const classes = useQuickFilterStyles();
-//     return <Chip className={classes.chip} label={translate(label)} />;
-// };
-
-// const PostFilter = props => (
-//     <Filter {...props}>
-//         <SearchInput source="q" alwaysOn />
-//         <TextInput
-//             source="title"
-//             defaultValue="Qui tempore rerum et voluptates"
-//         />
-//         <QuickFilter
-//             label="resources.posts.fields.commentable"
-//             source="commentable"
-//             defaultValue
-//         />
-//     </Filter>
-// );
-
-// const exporter = posts => {
-//     const data = posts.map(post => ({
-//         ...post,
-//         backlinks: lodashGet(post, 'backlinks', []).map(
-//             backlink => backlink.url
-//         ),
-//     }));
-//     jsonExport(data, (err, csv) => downloadCSV(csv, 'posts'));
-// };
+const colors = ["fe4848", "fe6c58", "fe9068", "feb478", "fed686"];
 
 const useStyles = makeStyles(theme => ({
     title: {
@@ -87,106 +53,125 @@ const useStyles = makeStyles(theme => ({
     publishedAt: { fontStyle: 'italic' },
 }));
 
-// const PostListBulkActions = memo(props => (
-//     <Fragment>
-//         <ResetViewsButton {...props} />
-//         <BulkDeleteButton {...props} />
-//         <BulkExportButton {...props} />
-//     </Fragment>
-// ));
+function customMarkerIcon(color, vector) {
+    const svgTemplate = `
+      <svg  xmlns="http://www.w3.org/2000/svg" transform="rotate(${vector})" viewBox="0 0 40 40" class="marker">
+        <path 
+            fill="#${color}" 
+            stroke="#fff"
+            d="M0,5L40,17L0,30L10,17zz"
+        />
+      </svg>`;
 
-// const usePostListActionToolbarStyles = makeStyles({
-//     toolbar: {
-//         alignItems: 'center',
-//         display: 'flex',
-//         marginTop: -1,
-//         marginBottom: -1,
-//     },
-// });
+    return new L.DivIcon({
+        className: "test",
+        html: svgTemplate,
+        iconSize: [40, 40],
+        iconAnchor: [12, 24],
+        popupAnchor: [7, -16],
+        style: { transform: [{ rotate: '180deg' }] }
+        });      
+}
 
-// const PostListActionToolbar = ({ children, ...props }) => {
-//     const classes = usePostListActionToolbarStyles();
-//     return (
-//         <div className={classes.toolbar}>
-//             {Children.map(children, button => cloneElement(button, props))}
-//         </div>
-//     );
-// };
+const MyMarkers = ({ data }) => {
+    return data.map((item, index) => (
+      <Marker
+        key={index}
+        icon={customMarkerIcon(colors[2], item.heading)}
+        position={{ lat: item.lat, lng: item.lng }}
+      >
+        <Popup>{item.title}</Popup>
+      </Marker>
+    ));
+};
 
-// const rowClick = (id, basePath, record) => {
-//     if (record.commentable) {
-//         return 'edit';
-//     }
+async function fetchDataReal (setMethod, lat, lon, zoom) {
+    fetch(`http://${process.env.REACT_APP_API_SERVER_FRONT}/api/v1/back/vehicle/map/ships?c_lat=${lat}&c_lon=${lon}&c_zoom=${zoom}`)
+    .then((response) => {
+      // console.log(response)
+      // console.log(response.json())
+      return response.json()
+    }).then((in_json) => {
+      let list_ships = in_json.results
+      let tabPoints = list_ships.map((curRow, curIndex) => (
+        { 
+          lat: curRow.lat, 
+          lng: curRow.lon,
+          heading: curRow.heading,
+          title: "title" 
+        }
+        ))
+      // console.log(tabPoints)
+      console.log("Data updated")
+      setMethod(tabPoints)
+    })
+  }
 
-//     return 'show';
-// };
+  const GetCoordinates = ({eventChange}) => {
+    const map = useMap();
+  
+    useEffect(() => {
+      if (!map) return;
+  
+      const legend = L.control({ position: "bottomleft" });
+  
+      const div = L.DomUtil.create("div", "legend");
+  
+      legend.onAdd = () => {
+        const { lat, lng } = map.getCenter();
+        const zoom = map.getZoom();
+  
+        L.DomEvent.disableClickPropagation(div);
+  
+        div.innerHTML = `center: ${lat.toFixed(5)}, ${lng.toFixed(
+          5
+        )} | zoom: ${zoom}`;
+  
+        return div;
+      };
+  
+      legend.addTo(map);
+  
+      map.on("dragend zoomend", () => {
+        const { lat, lng } = map.getCenter();
+        const zoom = map.getZoom();
+        fetchDataReal(eventChange, lat, lng, zoom)
+        // if (lat >= 52 && lat <= 53 && lng >= 20 && lng <= 22)   {
+        //   eventChange(points)
+        // }
+        div.innerHTML = `center: ${lat.toFixed(5)}, ${lng.toFixed(
+          5
+        )} | zoom: ${zoom}`;
+      });
+    }, [map]);
+  
+    return null;
+};
 
-// const PostPanel = ({ id, record, resource }) => (
-//     <div dangerouslySetInnerHTML={{ __html: record.body }} />
-// );
-
-const position = [55.706415, 37.426097];
-
-const NoneActions = props => (
-    <CardActions />
-)
+const position = [45.38, 29.68];
 
 const ShipList = props => {
     const classes = useStyles();
     const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
+
+    const [pointsShow, setPointsShow] = useState([])
+
+    useEffect(() => {
+        fetchDataReal(setPointsShow, position[0], position[1], 8)
+    },[])
+  
     return (
         <Grid container spacing={1}>
             <Grid item md={6}>
-                 <List
-                 {...props}
-                 actions={null}
-                 // bulkActionButtons={<PostListBulkActions />}
-                 // filters={<PostFilter />}
-                 // sort={{ field: 'published_at', order: 'DESC' }}
-                 // exporter={exporter}
-             >
-                 {/* {isSmall ? (
-                     <SimpleList
-                         primaryText={record => record.title}
-                         secondaryText={record => `${record.views} views`}
-                         tertiaryText={record =>
-                             new Date(record.published_at).toLocaleDateString()
-                         }
-                     />
-                 ) : ( */}
-                     <Datagrid optimized>
-                         <TextField source="id" />
-                         <TextField source="name" cellClassName={classes.title} />
-                         {/* <DateField
-                             source="published_at"
-                             cellClassName={classes.publishedAt}
-                         />
-
-                         <BooleanField
-                             source="commentable"
-                             label="resources.posts.fields.commentable_short"
-                             sortable={false}
-                         />
-                         <NumberField source="views" />
-                         <ReferenceArrayField
-                             label="Tags"
-                             reference="tags"
-                             source="tags"
-                             sortBy="tags.name"
-                            cellClassName={classes.hiddenOnSmallScreens}
-                             headerClassName={classes.hiddenOnSmallScreens}
-                         >
-                             <SingleFieldList>
-                                 <ChipField source="name" />
-                             </SingleFieldList>
-                         </ReferenceArrayField>
-                         <PostListActionToolbar>
-                             <EditButton />
-                             <ShowButton />
-                         </PostListActionToolbar> */}
-                     </Datagrid>
-                 {/* )} */}
-             </List>
+                <List
+                    actions={null}
+                    {...props}
+                >
+                    <Datagrid optimized>
+                        <TextField source="id" />
+                        <TextField source="name" cellClassName={classes.title} />
+                    </Datagrid>
+                </List>
             </Grid>
 
             <Grid item md={6}>
@@ -195,68 +180,21 @@ const ShipList = props => {
                     className="map-container"
                     preferCanvas
                     center={position}
-                    zoom={23}
+                    zoom={6}
                 >
-                    <TileLayer
-                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                        url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-                    />
+                    <LayersControl position="topright">
+                        <LayersControl.BaseLayer checked name="OpenStreetMap">
+                            <TileLayer
+                                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                            />
+                        </LayersControl.BaseLayer>
+                    </LayersControl>
+                    <MyMarkers data={pointsShow} />
+                    <GetCoordinates eventChange={setPointsShow}/>
                 </MapContainer>
             </Grid>
         </Grid>
-        // <Grid container>
-        //     <Grid>
-        //         <List
-        //         {...props}
-        //         // bulkActionButtons={<PostListBulkActions />}
-        //         // filters={<PostFilter />}
-        //         // sort={{ field: 'published_at', order: 'DESC' }}
-        //         // exporter={exporter}
-        //     >
-        //         {/* {isSmall ? (
-        //             <SimpleList
-        //                 primaryText={record => record.title}
-        //                 secondaryText={record => `${record.views} views`}
-        //                 tertiaryText={record =>
-        //                     new Date(record.published_at).toLocaleDateString()
-        //                 }
-        //             />
-        //         ) : ( */}
-        //             <Datagrid optimized>
-        //                 <TextField source="id" />
-        //                 <TextField source="name" cellClassName={classes.title} />
-        //                 {/* <DateField
-        //                     source="published_at"
-        //                     cellClassName={classes.publishedAt}
-        //                 />
-
-        //                 <BooleanField
-        //                     source="commentable"
-        //                     label="resources.posts.fields.commentable_short"
-        //                     sortable={false}
-        //                 />
-        //                 <NumberField source="views" />
-        //                 <ReferenceArrayField
-        //                     label="Tags"
-        //                     reference="tags"
-        //                     source="tags"
-        //                     sortBy="tags.name"
-        //                     cellClassName={classes.hiddenOnSmallScreens}
-        //                     headerClassName={classes.hiddenOnSmallScreens}
-        //                 >
-        //                     <SingleFieldList>
-        //                         <ChipField source="name" />
-        //                     </SingleFieldList>
-        //                 </ReferenceArrayField>
-        //                 <PostListActionToolbar>
-        //                     <EditButton />
-        //                     <ShowButton />
-        //                 </PostListActionToolbar> */}
-        //             </Datagrid>
-        //         {/* )} */}
-        //     </List>
-        //     </Grid>
-        // </Grid>
     );
 };
 
